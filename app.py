eb673efa0e7c0316167b7ac52319fa57033d5d3a
Vidyabharathi U <vidyabharathiu543@gmail.com>
2026-09-23 16:14:30 +0530

    Fix Gmail SMTP configuration

--- app.py
+++ app.py
@@ -48,8 +48,6 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 
 app.config["MAIL_PORT"] = 587
 
-app.config["MAIL_TIMEOUT"] = 30
-
 app.config["MAIL_USE_TLS"] = True
 
 app.config["MAIL_USE_SSL"] = False
